a9db7c8526ec5391b3ad6ab531fdb2fe965afb7b
vwylaw <29134440+vwylaw@users.noreply.github.com>
2022-10-09 09:55:31 +1100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
+  imports: [BrowserModule, HttpClientModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
